800c1bc99fd9d84b349731f04d61db5031df6721
Jake Oudakker <jake.t.oudakker@outlook.com>
2023-01-26 18:45:48 +0100

    Make frame with 16 moving particles

--- Particle.py
+++ Particle.py
@@ -4,7 +4,20 @@ Created on Thu Jan 26 15:23:21 2023
 
 @author: jaket
 """
-
+def new_pos(pos, posc):
+    if pos[0] + posc[0] > 10:
+        xnew = pos[0] + posc[0] - 10
+    elif pos[0] + posc[0] < 0:
+        xnew = pos[0] + posc[0] + 10
+    else:
+        xnew = pos[0] + posc[0]
+    if pos[1] + posc[1] >10:
+        ynew = pos[1] + posc[1] -10
+    elif pos[1] + posc[1] < 0:
+        ynew = pos[1] + posc[1] + 10
+    else:
+        ynew = pos[1] + posc[1]
+    return (xnew, ynew)
 class Particle():
     def __init__(self, pos, velocity):
         self.pos = pos
@@ -12,3 +25,10 @@ class Particle():
         
     def __str__(self):
         return "Position: (" + str((self.pos)[0]) + ", " + str((self.pos)[1]) + ")\nVelocity: (" + str((self.vel)[0]) + ", " + str((self.vel)[1]) + ")"
+    
+    
+    def update_pos(self, pos, posc):
+        pos_new = new_pos(pos, posc)
+        self.pos = pos_new
+        
+    
--- StatsMechProject.py
+++ StatsMechProject.py
@@ -7,21 +7,72 @@ Created on Tue Jan 24 15:17:36 2023
 
 from Particle import Particle
 import math
-from tkinter import *
+from tkinter import Tk, Canvas
+import time
+import random
 
 
 window_h_w = 700
-particles = []
+s = 70
+pr = 5
+speed = 5
+
+
+class Point():
+    def __init__(self, canvas, pos, color='red'):
+        self.canvas = canvas
+        self.xpos = int(s * pos[0])
+        self.ypos = int(s * pos[1])
+        self.color = color
+        self.id = canvas.create_oval(self.xpos-pr,self.ypos-pr,self.xpos+pr,self.ypos+pr, fill=color)
+    def draw(self):
+        self.canvas.move(self.id, 0, -1)
 
 def initialize_lattice():
+    particles = []
     for i in range(0, 16):
         particles.append(Particle((2*(i%4 + 1), 2*(math.trunc(i/4) + 1)), (0,0)))
     for part in particles:
         print(part)
+    return particles
+
+def update_position(particles):
+    for p in particles:
+        xr = random.random()
+        yr = random.random()
+        if xr > 0.66:
+            xc = 0.01 * speed
+        elif xr > 0.33:
+            xc = 0
+        else:
+            xc = -0.01 * speed
+        if yr > 0.66:
+            yc = 0.01 * speed
+        elif yr > 0.33:
+            yc = 0
+        else:
+            yc = -0.01 * speed
+        p.update_pos(p.pos, (xc, yc))
+    return particles
 
 def show_system(particles):
     window = Tk()
-    window.title('Hello Python')
+    c = Canvas(master=window, bg='black',height=window_h_w, width=window_h_w)
+    c.pack()
+
+    window.title('Lattice')
     window.geometry( str(window_h_w)+"x" + str(window_h_w) + "+10+10")
-    window.mainloop()
-    return ""
+    while True:
+        id_list = [0 for i in particles]
+        for i in range(0, len(particles)):
+            p = particles[i]
+            id_list[i] = Point(c, p.pos)
+        window.update_idletasks()
+        window.update()
+        time.sleep(0.01)
+        #for p in id_list:
+          #  c.delete(p)
+        c.delete('all')
+        print(len(particles))
+        particles = update_position(particles)
+    return "End"
