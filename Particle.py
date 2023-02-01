3c9cd688943edf829d59df8d065684db89bf73f5
Jake Oudakker <jake.t.oudakker@outlook.com>
2023-02-01 16:19:34 +0100

    A whole lot of changes and a few figures to look at for 4 different simulations

--- Particle.py
+++ Particle.py
@@ -4,17 +4,22 @@ Created on Thu Jan 26 15:23:21 2023
 
 @author: jaket
 """
+
+window_h_w = 700
+r0 = 40
+r0_w = window_h_w/r0
+
 def new_pos(pos, posc):
-    if pos[0] + posc[0] > 10:
-        xnew = pos[0] + posc[0] - 10
+    if pos[0] + posc[0] > r0_w:
+        xnew = pos[0] + posc[0] - r0_w
     elif pos[0] + posc[0] < 0:
-        xnew = pos[0] + posc[0] + 10
+        xnew = pos[0] + posc[0] + r0_w
     else:
         xnew = pos[0] + posc[0]
-    if pos[1] + posc[1] >10:
-        ynew = pos[1] + posc[1] -10
+    if pos[1] + posc[1] > r0_w:
+        ynew = pos[1] + posc[1] - r0_w
     elif pos[1] + posc[1] < 0:
-        ynew = pos[1] + posc[1] + 10
+        ynew = pos[1] + posc[1] + r0_w
     else:
         ynew = pos[1] + posc[1]
     return (xnew, ynew)
@@ -25,7 +30,7 @@ class Particle():
         self.vel = velocity
         
     def __str__(self):
-        return "Position: (" + str((self.pos)[0]) + ", " + str((self.pos)[1]) + ")\nVelocity: (" + str((self.vel)[0]) + ", " + str((self.vel)[1]) + ")"
+        return "Velocity: (" + str((self.vel)[0]) + ", " + str((self.vel)[1]) + ")"
     
     
     def update_pos(self, posc):
--- StatsMechProject.py
+++ StatsMechProject.py
@@ -5,96 +5,51 @@ Created on Tue Jan 24 15:17:36 2023
 @author: jaket
 """
 
-from Particle import Particle
+from Particle import Particle, r0, r0_w, window_h_w
 import math
 from tkinter import Tk, Canvas
 import time
 import random
-
-
-window_h_w = 700 # window height and width
-s = 40 # scale factor of lattice
-offset = 70*5-s*5 #offset of the lattice
-pr = 5  #point radius
-#speed = 5
-dt = .005 #time step
+#import msvcrt
+import matplotlib.pyplot as plt
+import copy
+import seaborn as sns
+from tqdm import trange, tqdm
 
 
 
-class Point():
-    def __init__(self, canvas, pos, color='red'):
-        self.canvas = canvas
-        self.xpos = int(s * pos[0] + offset)
-        self.ypos = int(s * pos[1] + offset)
-        self.color = color
-        self.id = canvas.create_oval(self.xpos-pr,self.ypos-pr,self.xpos+pr,self.ypos+pr, fill=color)
-    def draw(self):
-        self.canvas.move(self.id, 0, -1)
+#window_h_w = 700 # window height and width
+#r0 = 40 # scale factor of lattice
+#r0_w = window_h_w/r0
+pr = 10*(r0/70)  #point radius
+dt = .001 #time step
+speed_bin_size = 0.001
 
-def initialize_lattice():
-    particles = []
-    for i in range(0, 16):
-        particles.append(Particle((2*(i%4 + 1), 2*(math.trunc(i/4) + 1)), (0,0)))
-    for part in particles:
-        print(part)
-    return particles
 
-"""def update_position(particles):
-    for p in particles:
-        xr = random.random()
-        yr = random.random()
-        if xr > 0.66:
-            xc = 0.01 * speed
-        elif xr > 0.33:
-            xc = 0
-        else:
-            xc = -0.01 * speed
-        if yr > 0.66:
-            yc = 0.01 * speed
-        elif yr > 0.33:
-            yc = 0
-        else:
-            yc = -0.01 * speed
-        p.update_pos(p.pos, (xc, yc))
-    return particles"""
-
-def update_position(particles):
-    forces = calc_forces(particles)
-    for i in range(0, len(particles)):
-        p = particles[i]
-        new_x = p.pos[0] + (p.vel[0] * dt) + (forces[i][0]*(dt**2)/2)
-        new_y = p.pos[1] + (p.vel[1] * dt) + (forces[i][1]*(dt**2)/2)
-        xc = new_x - p.pos[0] 
-        yc = new_y - p.pos[1]
-        p.update_pos((xc, yc))
-    return particles
-
-def update_velocity(curr_particles, next_particles):
-    curr_forces = calc_forces(curr_particles)
-    next_forces = calc_forces(next_particles)
-    for i in range(0, len(curr_particles)):
-        curr_p = curr_particles[i]
-        new_vx = curr_p.vel[0] + ((next_forces[i][0] + curr_forces[i][0])*dt)/2
-        new_vy = curr_p.vel[1] + ((next_forces[i][1] + curr_forces[i][1])*dt)/2
-        next_particles[i].update_vel((new_vx, new_vy))
-    return next_particles
+######### Calculations Functions ##############
 
 def calc_forces(particles):
-    forces = [0 for i in particles]
+    forces = [(0,0) for p in particles]
     for i in range(0, len(particles)):
-        copy_particles = particles.copy()
-        reduced_particles = copy_particles[:i] + copy_particles[i+1:]
-        #print(len(reduced_particles))
-        force_x = 0
-        force_y = 0
-        for rp in reduced_particles:
-            dif_x = particles[i].pos[0] - rp.pos[0]
-            dif_y = particles[i].pos[1] - rp.pos[1]
-            force_x += f_x((dif_x, dif_y))
-            force_y += f_y((dif_x, dif_y))
-        forces[i] = (force_x, force_y)
+        for j in range(i+1, len(particles)):
+            if j != i:
+                dif_x = particles[i].pos[0] - particles[j].pos[0]
+                dif_y = particles[i].pos[1] - particles[j].pos[1]
+                if dif_x > r0_w/2:
+                   dif_x -= r0_w
+                elif dif_x < -r0_w/2:
+                    dif_x += r0_w
+                if dif_y > r0_w/2:
+                    dif_y -= r0_w
+                elif dif_y < -r0_w/2:
+                    dif_y += r0_w
+                force_x = f_x((dif_x, dif_y))
+                force_y = f_y((dif_x, dif_y))
+                forces[i] = (forces[i][0] + force_x, forces[i][1] + force_y)
+                forces[j] = (forces[j][0] - force_x, forces[j][1] - force_y)
+    #print(forces[0])
     return forces
-    
+    #return [(0,0) for p in particles]
     
 """
 Takes the difference in the x and y positions between 2 particles
@@ -112,42 +67,124 @@ def f_y(pos):
     return 12*pos[1]*((1/r2**7) - (1/r2**4))
 
 
-def calc_energy(particles):
+def calc_energy(particles, pprint = False):
+    energies = [(0,0) for p in particles] # kinetic, potential
+    for i in range(0, len(particles)):
+        for j in range(i+1, len(particles)):
+            if j != i:
+                dif_x = abs(particles[i].pos[0] - particles[j].pos[0])
+                dif_y = abs(particles[i].pos[1] - particles[j].pos[1])
+                if dif_x > r0_w/2:
+                   dif_x -= r0_w
+                elif dif_x < -r0_w/2:
+                    dif_x += r0_w
+                if dif_y > r0_w/2:
+                    dif_y -= r0_w
+                elif dif_y < -r0_w/2:
+                    dif_y += r0_w
+                energies[i] = (energies[i][0], energies[i][1] + ((1/(dif_x**2+dif_y**2)**6) - 2*(1/(dif_x**2+dif_y**2)**3)))
+        energies[i] = ((particles[i].vel[0]**2 + particles[i].vel[1]**2)/2 , energies[i][1])
     total_energy = 0
+    for e in energies:
+        total_energy += e[0] + e[1]
+    if pprint:
+        print(total_energy)
+    return total_energy
+
+
+########## Update Functions ##########
+
+def verlet_update_position(particles):
+    forces = calc_forces(particles)
     for i in range(0, len(particles)):
         p = particles[i]
-        copy_particles = particles.copy()
-        reduced_particles = copy_particles[:i] + copy_particles[i+1:]
-        pot_energy = 0
-        for rp in reduced_particles:
-            diff_x = p.pos[0] - rp.pos[0]
-            diff_y = p.pos[1] - rp.pos[1]
-            pot_energy += ((1/(diff_x**2+diff_y**2)**6) - (2/(diff_x**2+diff_y**2)**3))
-        kin_energy = (p.vel[0]**2 + p.vel[1]**2)/2
-        single_particle_energy = pot_energy + kin_energy
-        total_energy += single_particle_energy
-        #if i == 0:
-            #print(single_particle_energy, pot_energy, kin_energy)
-    print(total_energy)
-            
-            
-            
-
-def update_all(particles):
-    curr_particles = particles.copy()
-    particles = update_position(particles)
-    particles = update_velocity(curr_particles, particles)
+        new_x = p.pos[0] + (p.vel[0] * dt) + ((forces[i][0]*(dt*dt))/2)
+        new_y = p.pos[1] + (p.vel[1] * dt) + ((forces[i][1]*(dt*dt))/2)
+        xc = new_x - p.pos[0] 
+        yc = new_y - p.pos[1]
+        p.update_pos((xc, yc))
     return particles
 
-def show_system(particles):
+def verlet_update_velocity(curr_particles, next_particles):
+    curr_forces = calc_forces(curr_particles)
+    next_forces = calc_forces(next_particles)
+    for i in range(0, len(curr_particles)):
+        curr_p = curr_particles[i]
+        new_vx = curr_p.vel[0] + ((next_forces[i][0] + curr_forces[i][0])*dt)/2
+        new_vy = curr_p.vel[1] + ((next_forces[i][1] + curr_forces[i][1])*dt)/2
+        next_particles[i].update_vel((new_vx, new_vy))
+    return next_particles
+
+def euler_update_position(particles):
+    for i in range(0, len(particles)):
+        p = particles[i]
+        new_x = p.pos[0] + (p.vel[0] * dt)
+        new_y = p.pos[1] + (p.vel[1] * dt)
+        xc = new_x - p.pos[0] 
+        yc = new_y - p.pos[1]
+        p.update_pos((xc, yc))
+    return particles
+        
+def euler_update_velocity(curr_particles, next_particles):
+    curr_forces = calc_forces(curr_particles)
+    for i in range(0, len(curr_particles)):
+        curr_p = curr_particles[i]
+        new_vx = curr_p.vel[0] + curr_forces[i][0]*dt
+        new_vy = curr_p.vel[1] + curr_forces[i][1]*dt
+        next_particles[i].update_vel((new_vx, new_vy))
+    return next_particles
+
+def euler_update_all(particles):
+    curr_particles = particles.copy()
+    particles = euler_update_position(particles)
+    particles = euler_update_velocity(curr_particles, particles)
+    return particles
+            
+
+def verlet_update_all(particles):
+    curr_particles = particles.copy()
+    particles = verlet_update_position(particles)
+    particles = verlet_update_velocity(curr_particles, particles)
+    return particles
+
+def hybrid_1_update_all(particles):
+    curr_particles = particles.copy()
+    particles = euler_update_position(particles)
+    particles = verlet_update_velocity(curr_particles, particles)
+    return particles
+
+def hybrid_2_update_all(particles):
+    curr_particles = particles.copy()
+    particles = verlet_update_position(particles)
+    particles = euler_update_velocity(curr_particles, particles)
+    return particles
+
+def find_speed(particles, speed_dict):
+    for p in particles:
+        speed = p.vel[0]**2 + p.vel[1]**2
+        bin_number = math.trunc(speed/speed_bin_size)
+        if bin_number in speed_dict:
+            speed_dict[bin_number] += 1
+        else:
+            speed_dict[bin_number] = 1 
+    return speed_dict
+    
+######## Simulate and Run (Slow) ###########
+
+def show_system(particles, pause=False, steps = 40000):
     window = Tk()
     c = Canvas(master=window, bg='black',height=window_h_w, width=window_h_w)
     c.pack()
-
     window.title('Lattice')
     window.geometry( str(window_h_w)+"x" + str(window_h_w) + "+10+10")
     #frame = 0
-    while True:
+    #Elist = []
+    #dtlist = []
+    plist = []
+    #speed_dict = dict()
+    time_step = 0
+    run = True
+    while run:
         #print(frame)
         id_list = [0 for i in particles]
         for i in range(0, len(particles)):
@@ -155,12 +192,138 @@ def show_system(particles):
             id_list[i] = Point(c, p.pos)
         window.update_idletasks()
         window.update()
-        time.sleep(0.0001)
+        #time.sleep(0.00001)
         #for p in id_list:
           #  c.delete(p)
         c.delete('all')
         #print(len(particles))
-        calc_energy(particles)
-        particles = update_all(particles)
+        #Elist.append(calc_energy(particles))
+        #dtlist.append(time_step)
+        #speed_dict = find_speed(particles, speed_dict)
+        copyofparticles = copy.deepcopy(particles)
+        plist.append(copyofparticles)
+        #print(particles[0])
+        if pause:
+            [print(p) for p in particles]
+            inp = input()
+            if inp == 'y':
+                pause = False
+        if time_step > steps:
+            run = False
+            window.destroy()
+        particles = hybrid_1_update_all(particles)
         #frame +=1
-    return "End"
+        time_step+=1
+    return plist #Elist, dtlist, speed_ct
+
+########## Visuals ###########
+
+class Point():
+    def __init__(self, canvas, pos, color='red'):
+        self.canvas = canvas
+        self.xpos = int (r0 * pos[0])
+        self.ypos = int (r0 * pos[1])
+        self.color = color
+        self.id = canvas.create_oval(self.xpos-pr,self.ypos-pr,self.xpos+pr,self.ypos+pr, fill=color)
+    def draw(self):
+        self.canvas.move(self.id, 0, -1)
+
+
+######### Main Functions ##########
+
+def bake_sim(particles, steps = 40000):
+    plist = []
+    for i in trange(steps):
+        copyofparticles = copy.deepcopy(particles)
+        plist.append(copyofparticles)
+        particles = hybrid_1_update_all(particles)
+    return plist
+
+def resimulate(plist):
+    window = Tk()
+    c = Canvas(master=window, bg='black',height=window_h_w, width=window_h_w)
+    c.pack()
+    window.title('Lattice')
+    window.geometry( str(window_h_w)+"x" + str(window_h_w) + "+10+10")
+    for particles in tqdm(plist):
+        id_list = [0 for i in particles]
+        for i in range(0, len(particles)):
+            p = particles[i]
+            id_list[i] = Point(c, p.pos)
+        window.update_idletasks()
+        window.update()
+        c.delete('all')
+        time.sleep(0.00000000001)
+    window.destroy()
+    return "Finished"
+
+def initialize_lattice(edge, part_dist, vmag = 0):
+    center = r0_w/2
+    N = edge*edge
+    particles = [0 for i in range(0,N)]
+    lattice_width = (edge-1)*part_dist
+    lw_half = lattice_width/2
+    start = center - lw_half
+    for i in range(0, N):
+        particles[i] = Particle(((start + (part_dist*(i%edge))), (start + (part_dist*math.trunc(i/edge)))),(random.uniform(-1*vmag,vmag),random.uniform(-1*vmag, vmag)))
+    for part in particles:
+        print(part)
+    return particles
+
+###### Analysis #########
+
+def plot_energy(plist, calc = True):
+    if calc:
+        Elist = []
+        dtlist = []
+        time_step = 0
+        for particles in tqdm(plist):
+            Elist.append(calc_energy(particles))
+            dtlist.append(time_step)
+            time_step += 1
+    else:
+        Elist = plist
+    plt.scatter(dtlist, Elist)
+    plt.title("Total Energy over Time")
+    plt.ylabel("Energy (au)")
+    plt.xlabel("Time Steps")
+    return Elist
+
+def distr_energy(Elist):
+    fig = sns.displot(Elist, kind="hist", kde = True)
+    fig.set_axis_labels('Total Energy (au)', 'Counts')
+    
+def distr_speed(plist):
+    speeds = []
+    for particles in tqdm(plist):
+        for p in particles:
+            speeds.append(p.vel[0]**2 + p.vel[1]**2)
+    #plt.hist(speeds, 200)
+    #plt.title("Speed Distribution")
+    #plt.xlabel("Speed (au)")
+    #plt.ylabel("Frequency")
+    fig = sns.displot(speeds, kind='hist', kde=True)
+    fig.set_axis_labels('Speed (au)', 'Counts')
+    return speeds
+    
+
+###### Tests ##########
+def test_1_static():
+    ps = initialize_lattice(4, 2)
+    plist = bake_sim(ps)
+    return plist
+
+def test_1_fast():
+    ps = initialize_lattice(4, 2, vmag = 10)
+    plist = bake_sim(ps)
+    return plist
+
+def test_2_static():
+    ps = initialize_lattice(10, 1)
+    plist = bake_sim(ps)
+    return plist
+
+def test_2_fast():
+    ps = initialize_lattice(10, 1, vmag = 10)
+    plist = bake_sim(ps)
+    return plist
